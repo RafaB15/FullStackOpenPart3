f702fee10d1995ea5b3eee7a6c4d3979d8502133
Rafael Berenguel <rafaelberenguel15@gmail.com>
2023-12-24 17:09:43 -0300

    Finished 3.17

--- index.js
+++ index.js
@@ -23,29 +23,6 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-let phonebook = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
         response.json(persons)
@@ -84,7 +61,7 @@ const generateId = () => {
     return Math.floor(Math.random() * 1000000)
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     
     const body = request.body
 
@@ -97,7 +74,7 @@ app.post('/api/persons', (request, response) => {
             error: 'name missing'
         })
     }
-    
+
     const person = new Person({
         name: body.name,
         number: body.number,
@@ -106,6 +83,31 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    if (!body) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    } else if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 const PORT = process.env.PORT || 3001
